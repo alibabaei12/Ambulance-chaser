909b2a5e5ff07d2398fd8ee5b00bfd5aea4c80e8
Ali Babaei <albabaei@ucsc.edu>
2019-01-27 18:16:28 -0800

    first commit

---
+++ Practice/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed;
+    public Transform player;
+    private Rigidbody2D rb;
+
+
+    private void Start()
+    {
+          rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        float z = Mathf.Atan2((player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x)) *
+            Mathf.Rad2Deg - 90;
+
+        transform.eulerAngles = new Vector3(0, 0, z);
+
+        rb.AddForce(gameObject.transform.up * speed);
+
+    }
+}
---
+++ Practice/Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMobility : MonoBehaviour
+{
+    public float speed;
+    public float input;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate()
+    {
+        
+        //find the mouce posion and store it
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+        // get the rotation of the car
+        //quaternion lets us look at what we wanna look at
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        //set the rotation of our car to the rotaion of where we look
+        transform.rotation = rot;
+
+        //next line makes the car not shrink when moving the mouse away 
+        //from the car (only want z rotation and not x and y rotation)
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        //rigidbody2D.angularVelocity = 0;
+        
+        rb.angularVelocity = 0;
+
+        //making the car move forward
+ 
+        //going forward
+        input = Input.GetAxis("Vertical");
+        rb.AddForce(transform.up * speed * input);
+
+    }
+}
